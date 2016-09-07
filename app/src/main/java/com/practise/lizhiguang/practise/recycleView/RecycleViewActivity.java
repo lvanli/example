9eb9213692277b6033d6ba1ba4532aba1503de7b
lizhiguang <guofu_nan@163.com>
2016-09-07 08:57:13 +0800

    add recycle view examples

--- app/src/main/java/com/practise/lizhiguang/practise/recycleView/RecycleViewActivity.java
+++ app/src/main/java/com/practise/lizhiguang/practise/recycleView/RecycleViewActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.practise.lizhiguang.practise.R;
 
 public class RecycleViewActivity extends AppCompatActivity {
     RecyclerView mainView;
+    DividerItemDecoration mDecotarion = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +29,34 @@ public class RecycleViewActivity extends AppCompatActivity {
         mainView.setLayoutManager(new LinearLayoutManager(this));
         mainView.setAdapter(myAdapter);
         mainView.setItemAnimator(new DefaultItemAnimator());
-        mainView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+        mDecotarion = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST);
+        mainView.addItemDecoration(mDecotarion);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.recycle_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.recycle_menu_list:
+                if (mDecotarion != null)
+                    mainView.removeItemDecoration(mDecotarion);
+                mDecotarion = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST);
+                mainView.setLayoutManager(new LinearLayoutManager(this));
+                mainView.addItemDecoration(mDecotarion);
+                break;
+            case R.id.recycle_menu_grid:
+                if (mDecotarion != null)
+                    mainView.removeItemDecoration(mDecotarion);
+                mDecotarion = new DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL_LIST);
+                mainView.setLayoutManager(new StaggeredGridLayoutManager(5,StaggeredGridLayoutManager.HORIZONTAL));
+                mainView.addItemDecoration(mDecotarion);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
